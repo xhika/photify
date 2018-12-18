c63df1a94863a783a579b1ca626854cbe32ab50b
Laureta Dzika <xhika_l@hotmail.com>
2018-12-18 12:01:55 +0100

    looping out post on profile page

--- app/users/profile.php
+++ app/users/profile.php
@@ -4,7 +4,7 @@
 	<article>
 		<div class="max-w-md mx-auto text-center uppercase">
  	   		<h1 class="m-0 p-4">Welcome</h1>
-			<span class="bg-teal py-2 px-3 font-semibold rounded text-white">Username</span>
+			<span class="bg-teal py-2 px-3 font-semibold rounded text-white"><?= $_SESSION['username']; ?></span>
 			<img class="mt-6 border block mx-auto h-32 rounded-full shadow-lg" src="/img/avatar1.png">
 			<div class="mt-4">
 				<button class="bg-teal py-3 px-4 rounded font-semibold text-white mt-2">Profile</button>
@@ -15,7 +15,13 @@
 	</article>
 </div>
 	<h2 class="max-w-lg mx-auto text-center pt-6 pb-6">Posts</h2>
-
+	<?php  require __DIR__.'/../posts/view.php'; ?>
+		<?php foreach ($posts as $post) : ?>
+			<div class="bg-teal pt-1 mb-6">
+				<h1 class="pl-4 pr-4 text-white font-semibold"><?= $post['title']; ?></h1>
+				<p class="pl-4 pr-4 pb-4 text-white leading-normal"><?= $post['content']; ?></p>
+			</div>
+		<?php endforeach; ?>
 
 
 <?php require __DIR__.'/../../views/footer.php'; ?>

f2dc82ee3233ac7319b50ca9d5736c7d16343d86
Laureta Dzika <xhika_l@hotmail.com>
2018-12-19 15:47:02 +0100

    changes in db and small design fixes

--- app/users/profile.php
+++ app/users/profile.php
@@ -6,8 +6,8 @@
  	   		<h1 class="m-0 mb-2 p-4 text-teal">Welcome</h1>
 			<span class="bg-teal mb-2 py-2 px-3 font-semibold rounded text-white"><?= $_SESSION['username']; ?></span>
 			<img class="mt-6 border block mx-auto h-32 rounded-full shadow-lg" src="/img/avatar1.png">
-			<h2 class="font-semibold text-teal mt-4 mb-4">Bio</h2>
-			<div class="bg-grey-light h-auto w-1/4 rounded mx-auto p-4 normal-case">
+			<h2 class="tracking-wide normal-case font-normal text-teal-dark text-xl mt-4 mb-4">Biography</h2>
+			<div class="bg-grey-light h-auto w-1/4 rounded mx-auto p-4 normal-case font-light">
 			<?php
 				$bio =  getUserInfo($pdo);
 				echo $bio['bio'];

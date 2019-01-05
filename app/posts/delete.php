9690886247a9bf8466741be70ea5ed23af85a534
Laureta Dzika <xhika_l@hotmail.com>
2019-01-05 15:34:55 +0100

    delete posts added

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -6,4 +6,26 @@ require __DIR__.'/../autoload.php';
 
 // In this file we delete new posts in the database.
 
-redirect('/');
+try {
+	$postId = $_GET['id'];
+	$username = $_SESSION['username'];
+
+	$sql = 'DELETE FROM posts WHERE user_id = :username AND id = :postId';
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':postId', $postId, PDO::PARAM_INT);
+	$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+	$stmt->execute();
+
+	if (!stmt) {
+		$_SESSION['error'] = 'There was an error, please try again.';
+		redirect('/app/users/profile.php');
+		exit;
+	} else {
+		$_SESSION['success'] = 'Post was successfully deleted.';
+		redirect('/app/users/profile.php');
+		exit;
+	}
+
+} catch (Exception $e) {
+	echo 'Something went wrong with the connection: ' . $e->getMessage();
+}

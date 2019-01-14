6bfa36ee254393c4bcb511eccd841753f849f00d
Laureta Dzika <xhika_l@hotmail.com>
2019-01-14 09:16:21 +0100

    added preview image on new post

--- views/new-post.php
+++ views/new-post.php
@@ -3,6 +3,7 @@ require __DIR__.'/../views/header.php';
 ?>
 <div class="bg-grey-lighter p-6 h-screen">
 	<form method="post" class="text-center" enctype="multipart/form-data" action="/app/posts/store.php">
+		<img class="border block mx-auto w-full h-64 bg-auto" src="#>">
 		<label for=title class="block font-bold uppercase tracking-wide pt-3">Subject:</label>
 		<input type="title" name="title" class="w-3/4 bg-grey-light rounded py-2 border-solid border-black my-2">
 		<label for=content class="block font-bold uppercase tracking-wide pt-3">Post:</label>
@@ -11,7 +12,7 @@ require __DIR__.'/../views/header.php';
 
 			<div>
 				<label for="image" class="mt-4 py-2 px-5 bg-teal m-2 rounded text-white font-thin"><i class="far fa-image"></i>
-				<input class="hidden" type="file" name="image" id="image" required>
+				<input class="hidden" type="file" name="image" id="image" onchange="previewFile()" required>
 				</label>
 				<button type="submit" name="submit" class="mt-4 py-2 px-5 bg-teal m-2 rounded text-white font-thin">Post</button>
 			</div>

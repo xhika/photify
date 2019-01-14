2686dc349c4e2be80773e113a32055cb25bc56b6
Laureta Dzika <xhika_l@hotmail.com>
2019-01-14 18:04:33 +0100

    minor styling
    edit & new post can now upload pictures through clicking on image container instead of button

--- views/edit-view.php
+++ views/edit-view.php
@@ -40,14 +40,15 @@ foreach ($posts as $post) : ?>
 <div class="bg-grey-lighter p-6 h-screen">
 	<form method="post" class="text-center" enctype="multipart/form-data">
 		<div class="mb-8 md:max-w-sm mx-auto">
-			<img class="border block mx-auto w-full h-64 bg-auto" src="/img/<?= $post['filepath']; ?>">
+			<label for="image" class="mt-4 m-2 hover:bg-teal-lightest">
+				<img class="border block mx-auto w-full h-64 bg-auto" src="/img/<?= $post['filepath']; ?>">
+			</label>
 			<label for=title class="block font-bold uppercase tracking-wide pt-3">Subject:</label>
-			<input type="title" name="title" class="w-3/4 bg-grey-light rounded py-2 border-solid border-black my-2" value="<?= $post['title'] ;?>">
+			<input type="title" name="title" class="w-3/4 bg-grey-light rounded py-2 my-2 focus:bg-white" value="<?= $post['title'] ;?>">
 			<label for=content class="block font-bold uppercase tracking-wide pt-3">Post:</label>
-			<textarea name="content" class="w-3/4 bg-grey-light my-2 mx-auto" cols="25" rows="5"><?= $post['content']; ?></textarea>
+			<textarea name="content" class="w-3/4 bg-grey-light my-2 mx-auto focus:bg-white" cols="25" rows="5"><?= $post['content']; ?></textarea>
 		</div>
 		<div>
-			<label for="image" class="mt-4 py-2 px-5 bg-teal m-2 rounded text-white font-thin"><i class="far fa-image"></i>
 			<input class="hidden" type="file" name="image" id="image" onchange="previewFile()" required>
 			</label>
 			<button type="submit" name="edit" class="mt-4 py-2 px-5 bg-teal m-2 rounded text-white font-thin">Update</button>
--- views/new-post.php
+++ views/new-post.php
@@ -3,15 +3,18 @@ require __DIR__.'/../views/header.php';
 ?>
 <div class="bg-grey-lighter p-6 h-screen">
 	<form method="post" class="text-center" enctype="multipart/form-data" action="/app/posts/store.php">
-		<img class="border block mx-auto w-full h-64 bg-auto" src="#>">
-		<label for=title class="block font-bold uppercase tracking-wide pt-3">Subject:</label>
-		<input type="title" name="title" class="w-3/4 bg-grey-light rounded py-2 border-solid border-black my-2">
-		<label for=content class="block font-bold uppercase tracking-wide pt-3">Post:</label>
-			<textarea name="content" class="w-3/4 bg-grey-light my-2 mx-auto" cols="25" rows="5"></textarea>
+		<div class="mb-8 md:max-w-sm mx-auto">
+			<label for="image" class="mt-4 m-2 hover:bg-teal-lightest">
+				<img class="border block mx-auto w-full h-64 bg-auto" src="/img/img-placeholder.png">
+			</label>
+			<label for=title class="block font-bold uppercase tracking-wide pt-3">Subject:</label>
+			<input type="title" name="title" class="w-3/4 bg-grey-light rounded py-2 my-2 focus:bg-white">
+			<label for=content class="block font-bold uppercase tracking-wide pt-3">Post:</label>
+				<textarea name="content" class="w-3/4 bg-grey-light my-2 mx-auto focus:bg-white" cols="25" rows="5"></textarea>
+		</div>
 
 
 			<div>
-				<label for="image" class="mt-4 py-2 px-5 bg-teal m-2 rounded text-white font-thin"><i class="far fa-image"></i>
 				<input class="hidden" type="file" name="image" id="image" onchange="previewFile()" required>
 				</label>
 				<button type="submit" name="post" class="mt-4 py-2 px-5 bg-teal m-2 rounded text-white font-thin">Post</button>

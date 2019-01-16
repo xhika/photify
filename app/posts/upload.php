0d21e097670accefadaaaf921d1b56e460f010b2
Laureta Dzika <xhika_l@hotmail.com>
2019-01-16 13:11:24 +0100

    fix

--- app/posts/upload.php
+++ app/posts/upload.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require PHOTOIFY_PATH.'/views/upload-view.php';
+require __DIR__.'/../../views/upload-view.php';
 include_once __DIR__.'/../autoload.php';
 
 // Here we upload filepath to db.

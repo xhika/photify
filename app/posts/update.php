a3388b6c0d31684ae484472416c815cee5d2f975
Laureta Dzika <xhika_l@hotmail.com>
2019-01-19 14:07:03 +0100

    fix redirection on update

--- app/posts/update.php
+++ app/posts/update.php
@@ -27,35 +27,35 @@ try {
 		$filename = uniqid().'.'.$extension;
 		$path .= $filename;
 
-	if (move_uploaded_file($image['tmp_name'], $path)) {
+		if (move_uploaded_file($image['tmp_name'], $path)) {
 
-		$sql = 'UPDATE posts SET title = :title, content = :content, filepath = :image, date = :date WHERE id = :editId AND user_id = :memberId';
+			$sql = 'UPDATE posts SET title = :title, content = :content, filepath = :image, date = :date WHERE id = :editId AND user_id = :memberId';
 
-				$stmt = $pdo->prepare($sql);
-				$stmt->bindParam(':title', $title, PDO::PARAM_STR);
-				$stmt->bindParam(':content', $content, PDO::PARAM_STR);
-				$stmt->bindParam(':image', $filename, PDO::PARAM_STR);
-				$stmt->bindParam(':date', $date, PDO::PARAM_STR);
-				$stmt->bindParam(':editId', $editId, PDO::PARAM_INT);
-				$stmt->bindParam(':memberId', $memberId, PDO::PARAM_STR);
-				$stmt->execute();
+			$stmt = $pdo->prepare($sql);
+			$stmt->bindParam(':title', $title, PDO::PARAM_STR);
+			$stmt->bindParam(':content', $content, PDO::PARAM_STR);
+			$stmt->bindParam(':image', $filename, PDO::PARAM_STR);
+			$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+			$stmt->bindParam(':editId', $editId, PDO::PARAM_INT);
+			$stmt->bindParam(':memberId', $memberId, PDO::PARAM_STR);
+			$stmt->execute();
 
-				if (!$stmt) {
-		    		die(var_dump($pdo->errorInfo()));
-				}
-
-				if($stmt->rowCount() >= 1) {
-					$_SESSION['success'] = 'Edit successful.';
-					redirect('/../../feed.php');
-					exit;
-
-				} else {
-					$_SESSION['error'] = 'There was an error.';
-					redirect('/../../feed.php');
-					exit;
-				}
+			if (!$stmt) {
+				die(var_dump($pdo->errorInfo()));
 			}
 		}
+
+		if($stmt->rowCount() >= 1) {
+			$_SESSION['success'] = 'Edit successful.';
+			redirect('/../../feed.php');
+			exit;
+
+		} else {
+			$_SESSION['error'] = 'There was an error.';
+			redirect('/../../feed.php');
+			exit;
+		}
+	}
 } catch (PDOException $e) {
 	echo "Something went wrong with loading posts: " . $e->getMessage();
 }
